78ba6dfeb16b510b8fed6dabbdfef3fb2dd7a4d8
theolamide <ajadidan@gmail.com>
2025-04-23 08:37:32 -0400

    video section added

---
+++ src/components/home/video_iframe_section.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+interface VideoIframeProps {
+	src: string; // URL of the video
+	title: string; // Title for accessibility
+	width?: string; // Width of the iframe (default: "100%")
+	height?: string; // Height of the iframe (default: "315px")
+	allowFullScreen?: boolean; // Allow fullscreen mode (default: true)
+	allow?: string; // Additional allow attributes for the iframe (default: "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture")
+	referrerPolicy?: React.HTMLAttributeReferrerPolicy; // Referrer policy for the iframe (default: "strict-origin-when-cross-origin")
+}
+
+const VideoIframeSection: React.FC<VideoIframeProps> = ({
+	src,
+	title,
+	width = '100%',
+	height = '315px',
+	allowFullScreen = true,
+	allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture',
+	referrerPolicy = 'strict-origin-when-cross-origin',
+}) => {
+	return (
+		<div>
+			<iframe
+                style={{margin: '0 auto'}} //Olamide TODO: Make this a styled component and pass in a prop to specify middle, left, or right justification.
+				src={src}
+				title={title}
+				width={width}
+				height={height}
+				allowFullScreen={allowFullScreen}
+				frameBorder="0"
+				allow={allow}
+				referrerPolicy={referrerPolicy}
+			></iframe>
+		</div>
+	);
+};
+
+export default VideoIframeSection;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,20 @@ import FeatureHighlightsSection from '../components/home/feature_highlights_sect
 import TileCardSection from '../components/home/tile_card_section';
 import CalendarIntegrationSection from '../components/home/integration_section';
 import HeroSection from '../components/home/hero_section';
+import VideoIframeSection from '../components/home/video_iframe_section';
 
 function Home() {
 	return (
 		<>
 			<PersonaCarousel />
+			<VideoIframeSection
+				src="https://www.youtube.com/embed/N3L49xMBZ60?si=BmQ0wHBvThDCh5Zc"
+				title="YouTube video player"
+				width="1200"
+				height="808"
+				allowFullScreen={true}
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			/>
 			<FeatureHighlightsSection />
 			<TileCardSection />
 			<CalendarIntegrationSection />
