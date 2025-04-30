8df79c74c46d389572e316e17107a8275f14172a
jerbio <jeromebiotidara@gmail.com>
2025-04-30 02:45:40 -0600

    Implementation around i18n

---
+++ src/i18n/config.ts
@@ -0,0 +1,21 @@
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import entranslation from './en/translations.json';
+import esTranslation from './es/translations.json';
+
+
+i18next.use(initReactI18next).init({
+  lng: 'en', // if you're using a language detector, do not define the lng option
+  debug: true,
+  resources: {
+    en: {
+      translation: entranslation,
+    },
+    es: {
+      translation: esTranslation,
+    },
+  },
+  // if you see an error like: "Argument of type 'DefaultTFuncReturn' is not assignable to parameter of type xyz"
+  // set returnNull to false (and also in the i18next.d.ts options)
+  // returnNull: false,
+});
