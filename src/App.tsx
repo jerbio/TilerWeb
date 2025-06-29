2dbd7602ac513a8a2b3977bc2e7a7037e5a35c7a
theolamide <ajadidan@gmail.com>
2025-06-29 17:39:10 -0400

    setting up the global state

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,18 @@ import Features from './pages/Features';
 import { Toaster } from 'sonner';
 
 const App: React.FC = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Layout />}>
-					<Route index element={<Home />} />
-					<Route path="/features" element={<Features />} />
-				</Route>
-			</Routes>
-			<Toaster position="bottom-left" theme="system"/>
-		</BrowserRouter>
-	);
-}
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="/features" element={<Features />} />
+                </Route>
+            </Routes>
+            <Toaster position="bottom-left" theme="system" />
+        </BrowserRouter>
+    );
+};
 
 export default App;
