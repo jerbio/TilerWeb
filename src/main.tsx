748817321dcdf96095af6bf4339b2ef017e2dc4c
Okoye Charles <okoyecharles509@gmail.com>
2025-08-14 15:53:01 +0100

    refactor: organize styles for dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { createRoot } from 'react-dom/client';
 import './i18n/config.ts';
 
 createRoot(document.getElementById('root')!).render(<App />);
