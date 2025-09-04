40e05ded9dd4ac38b89ea5b13c862b7410742f14
Okoye Charles <okoyecharles509@gmail.com>
2025-09-04 17:44:40 +0100

    feat: set user id when persona is selected

--- src/components/home/persona_carousel/persona_card_expanded.tsx
+++ src/components/home/persona_carousel/persona_card_expanded.tsx
@@ -10,6 +10,7 @@ import Chat from '@/core/common/components/chat/chat';
 import useIsMobile from '@/core/common/hooks/useIsMobile';
 import { PersonaUsers, PersonaUserSetter } from '@/core/common/hooks/usePersonaUsers';
 import { personaService } from '@/services';
+import useAppStore from '@/global_state';
 
 type PersonaExpandedCardProps = {
   persona: Persona;
@@ -32,23 +33,36 @@ const PersonaCardExpanded: React.FC<PersonaExpandedCardProps> = ({
   const isDesktop = !useIsMobile(parseInt(palette.screens.lg, 10));
   const showChat = isDesktop || mobileChatVisible;
   const personaUserId = personaUsers[persona.id]?.userId || null;
+  const userInfo = useAppStore((state) => state.userInfo);
+  const setUserInfo = useAppStore((state) => state.setUserInfo);
 
   function onMobileCollapse() {
     setMobileChatVisible(false);
   }
 
+  function setUserId(userId: string) {
+		if (userInfo) {
+			setUserInfo({ ...userInfo, id: userId });
+		}
+	}
+
   async function getPersonaUser() {
     try {
       const personaUser = await personaService.createAnonymousUser(persona);
       setPersonaUser(persona.id, personaUser.anonymousUser.id);
+			setUserId(personaUser.anonymousUser.id!);
     } catch (error) {
       console.error("Couldn't create profile for persona: ", error);
     }
   }
 
   useEffect(() => {
-    if (expanded && !personaUserId) {
-      getPersonaUser();
+    if (expanded) {
+      if (!personaUserId) {
+        getPersonaUser();
+      } else {
+				setUserId(personaUserId);
+      }
     }
   }, [expanded]);
 
