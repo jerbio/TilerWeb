f7f6574e3b2d26396f1dc1a933e73955c7dcc631
Okoye Charles <okoyecharles509@gmail.com>
2025-11-07 18:19:33 +0100

    Revert "fix: due info appears on future events"
    This reverts commit 77994c5bd007e5d0064b7957bd680b1439660060.

--- src/core/common/components/calendar/calendar_event_info.tsx
+++ src/core/common/components/calendar/calendar_event_info.tsx
@@ -54,11 +54,7 @@ const CalendarEventInfo: React.FC<CalendarEventInfoProps> = ({ event, onClose })
         </div>
         <div className="title">
           <h2>{event.name}</h2>
-					{
-						dayjs().isBefore(dayjs(event.start)) ? (
-							<span>Due in {getEventDueIn(event)}</span>
-						) : null
-					}
+          <span>Due in {getEventDueIn(event)}</span>
         </div>
         <button onClick={onClose}>
           <X size={16} color={eventColor.setLightness(0.5).toHex()} />
