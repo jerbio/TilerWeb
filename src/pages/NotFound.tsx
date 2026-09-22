f843d7f89982f7a8c19471b485c210745f531e21
jerbio <jeromebiotidara@gmail.com>
2026-09-22 04:40:06 +0000

    Conversion lead addition for reddit

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -86,8 +86,8 @@ const NotFound: React.FC = () => (
 			<Code>404</Code>
 			<Title>Page not found</Title>
 			<Description>
-				We couldn't find the page you're looking for. It may have moved, or the link might
-				be out of date.
+				We couldn&apos;t find the page you&apos;re looking for. It may have moved, or the
+				link might be out of date.
 			</Description>
 			<HomeLink href="/">Back to Home</HomeLink>
 		</Wrapper>
